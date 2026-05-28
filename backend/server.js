b719faa85073f0ed7808032377d88ed041059719
GuettoX <comercial.buscafree@gmail.com>
2026-05-28 11:20:43 -0300

    Config Railway

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.get('/', (req, res) => {
 
 app.use('/generate-script', generateScriptRoute)
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
